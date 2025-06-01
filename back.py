8ab3ebad0719caac17d06de9a4cf5fdd1ef482b6
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2025-06-01 16:07:44 +0700

    FINISH_CREATE_BACK

--- back.py
+++ back.py
@@ -1,15 +1,22 @@
-import aiohttp
-import asyncio
-import APIKEY
+def back_main():
+    import aiohttp
+    import asyncio
+    import APIKEY
 
-async def back():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.api-ninjas.com/v1/cryptoprice?symbol=LTCBTC', headers=APIKEY.HEADERS) as resp:
-            print(resp.status)
-            print(await resp.text())
+    async def back():
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.api-ninjas.com/v1/bitcoin', headers=APIKEY.HEADERS) as resp:
+                if resp.status == 200:
+                    JSON_BITCOIN = await resp.json()
+                    BTCINFO = dict(JSON_BITCOIN)
+                    BTCPRICE = BTCINFO["price"]
+                else:
+                    print("NOT CONNECT")
+            
+            
 
             
-asyncio.run(back())
+    asyncio.run(back())
 
 
 
